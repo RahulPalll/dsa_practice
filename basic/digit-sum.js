740fededb6b3a77225a5fff56c04d171c06fbf44
Rahul Pal <rahul@Rahuls-MacBook-Pro.local>
2024-01-28 18:16:52 +0530

    final commit with testing

--- basic/digit-sum.js
+++ basic/digit-sum.js
@@ -1,14 +1,15 @@
 function sumOfDigits(num){
-     let sum = 0;
+    let sum = 0;
     while(num>0){
         sum += num%10;
         num = Math.floor(num / 10);
 
     }
     return sum;
-   
 }
 
-
+console.log(sumOfDigits(10));
+console.log(sumOfDigits(8790));
+console.log(sumOfDigits(1287));
 
 
