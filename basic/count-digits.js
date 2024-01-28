506f1da1d1fa4119175bfab11aac77fdd7380ee4
Rahul Pal <rahul@Rahuls-MacBook-Pro.local>
2024-01-28 23:56:14 +0530

    count of digit s of a number

--- basic/count-digits.js
+++ basic/count-digits.js
@@ -1,3 +1,17 @@
 function countNumberOfDigits(num){
-    
+    let count =0;
+   while(num>0){
+    count++;
+    num=Math.floor(num/10);
+
+   }
+   return count;
 }
+
+function numberOfDigits(num){
+    let numLength = num.toString().length;
+    return numLength;
+}
+
+console.log(countNumberOfDigits(6787));
+console.log(numberOfDigits(6787));
