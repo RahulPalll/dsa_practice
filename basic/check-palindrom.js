c020bb82a4d13f38382fed670b0ff5215e792b14
Rahul Pal <rahul@Rahuls-MacBook-Pro.local>
2024-01-29 01:02:12 +0530

    number palindrome

--- basic/check-palindrom.js
+++ basic/check-palindrom.js
@@ -11,5 +11,33 @@ function checkPalindrom(x) {
   }
 }
 
+function palindrom(x) {
+  if (x < 0) {
+    return false;
+  }
+  let newNumber = x;
+  let reverseNumber = "";
+  while (x > 0) {
+    reverseNumber += x % 10;
+    x = Math.floor(x / 10);
+  }
+  reverseNumber = +reverseNumber;
+
+  return reverseNumber === newNumber;
+}
+
+let isPalindrome = function (x) {
+  let copyNum = x,
+    reverseNum = 0;
+
+  while (copyNum > 0) {
+    const lastDigit = copyNum % 10;
+    reverseNum = reverseNum * 10 + lastDigit;
+    copyNum = Math.floor(copyNum / 10);
+  }
+
+  return x === reverseNum;
+};
+
 console.log(checkPalindrom("666"));
-// console.log(palindrom("666"));
+console.log(palindrom(666));
